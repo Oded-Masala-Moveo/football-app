7cb6609af6899d9ff964bec3ad2c5af29edc6610
Oded Masala <odedm@moveo.co.il>
2023-03-29 11:33:00 +0300

    first-commit build football app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LeagueTabsComponent } from './league-tabs/league-tabs.component';
+import { TeamsListComponent } from './teams-list/teams-list.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LeagueTabsComponent,
+    TeamsListComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/league-tabs/league-tabs.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-league-tabs',
+  templateUrl: './league-tabs.component.html',
+  styleUrls: ['./league-tabs.component.scss']
+})
+export class LeagueTabsComponent {
+
+}
